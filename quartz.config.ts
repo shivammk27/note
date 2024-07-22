e14a8e28072a9177c6c20361535db7a483f0256d
twopieare <skushwaha2706@gmail.com>
2024-07-22 15:43:53 +0530

    Quartz sync: Jul 22, 2024, 3:43 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -22,8 +22,8 @@ const config: QuartzConfig = {
       fontOrigin: "googleFonts",
       cdnCaching: true,
       typography: {
-        header: "Schibsted Grotesk",
-        body: "Source Sans Pro",
+        header: "Baskerville",
+        body: "Times New Roman",
         code: "IBM Plex Mono",
       },
       colors: {
